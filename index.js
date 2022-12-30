3c3c84855d9190c534d1c90743f785b72c6c64df
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-12-30 12:36:07 +0600

    taskCount-api-created

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
 //mongodb
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h5qu391.mongodb.net/?retryWrites=true&w=majority`;
@@ -36,10 +35,6 @@ async function run(){
         res.send(tasks);
       });
 
-      
-
-
-
       // delete api
       app.delete("/allTask/:id", async (req, res) => {
         const id = req.params.id;
@@ -48,24 +43,51 @@ async function run(){
         res.send(task);
       });
 
-      // mark completed
+      //update task
+      app.put("/allTask/:id" , async(req , res)=>{
+        const {id} = req.params
+        const taskData = req.body
+        console.log(taskData);
+        const updatedTask = await taskCollections.updateOne({_id : ObjectId(id)} , {$set : taskData})
+        res.send(updatedTask)
+     
+      })
 
+      // mark completed
       app.put("/allTask/update/:id" , async(req , res)=>{
         const id = req.params.id
         const filter = { _id: ObjectId(id) };
         const options = { upsert: true };
         const updatedDoc = {
             $set: {
-              status: 'completed'
+              status : { status: status === 'incomplete' ? 'complete' : 'incomplete' }
             },
           };
-    
           const result = await taskCollections.updateOne(
             filter,
             updatedDoc,
             options
           );
           res.send(result);
+
+      })
+
+      //get task count
+      app.get('/allTask/count' , async(req , res)=>{
+           
+        const email = req.query.email
+           //get total task count
+           const totalTask = await taskCollections.count({email : email})
+           //get total completed task
+           const completedTask = await taskCollections.count({email : email , status : "completed"})
+           //get total incomplete task 
+           const incompleteTask = await taskCollections.count({email : email , status : "incomplete"})
+
+           res.send({
+            totalTask,
+            completedTask,
+            incompleteTask
+           })
 
       })
 
